8e1cc523dd09cc82f515fb2f03a7acc35b9edd43
Immo Landwerth <immol@microsoft.com>
2020-07-21 00:16:24 -0700

    Improve sorting of issues
    We sort by:
    
        1. blocking
        2. no milestone
        3. versioned milestone
        4. alpha numeric milestone
        5. created date

--- ApiReview.Shared/ApiReviewIssue.cs
+++ ApiReview.Shared/ApiReviewIssue.cs
@@ -32,10 +32,40 @@ namespace ApiReview.Shared
         public int CompareTo(ApiReviewIssue other)
         {
             var result = -IsBlocking.CompareTo(other.IsBlocking);
-            if (result == 0)
-                result = CreatedAt.CompareTo(other.CreatedAt);
+            if (result != 0)
+                return result;
 
-            return result;
+            result = CompareMilestone(Milestone, other.Milestone);
+            if (result != 0)
+                return result;
+
+            return CreatedAt.CompareTo(other.CreatedAt);
+        }
+
+        private static int CompareMilestone(string x, string y)
+        {
+            static bool IsNone(string m) => string.IsNullOrEmpty(m) || m == ApiReviewConstants.NoMilestone;
+
+            if (IsNone(x) && IsNone(y))
+                return 0;
+
+            if (IsNone(x))
+                return -1;
+
+            if (IsNone(y))
+                return 1;
+
+            var xIsVersion = Version.TryParse(x, out var xVersion);
+            var yIsVersion = Version.TryParse(y, out var yVersion);
+
+            var result = -xIsVersion.CompareTo(yIsVersion);
+            if (result != 0)
+                return result;
+
+            if (xIsVersion && yIsVersion)
+                return xVersion.CompareTo(yVersion);
+
+            return string.Compare(x, y, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
