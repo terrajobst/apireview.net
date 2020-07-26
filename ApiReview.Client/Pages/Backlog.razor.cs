b31bd0b395801790266de665dc1ba916fdf6b688
Immo Landwerth <immol@microsoft.com>
2020-07-26 13:49:56 -0700

    Move code from razor views to partial classes

---
+++ ApiReview.Client/Pages/Backlog.razor.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using ApiReview.Client.Services;
+using ApiReview.Shared;
+
+namespace ApiReview.Client.Pages
+{
+    [Authorize]
+    public partial class Backlog : IDisposable
+    {
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        private IssueService IssueService { get; set; }
+
+        private IReadOnlyList<ApiReviewIssue> _issues;
+        private SortedDictionary<string, bool> _milestones;
+        private readonly HashSet<ApiReviewIssue> _checkedIssues = new HashSet<ApiReviewIssue>();
+
+        public string Filter { get; set; }
+        public IEnumerable<ApiReviewIssue> Issues => _issues ?? Array.Empty<ApiReviewIssue>();
+        public IEnumerable<ApiReviewIssue> VisibleIssues => Issues.Where(IsVisible);
+        public IEnumerable<ApiReviewIssue> SelectedIssues => VisibleIssues.Where(_checkedIssues.Contains);
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+            IssueService.Changed += IssuesChanged;
+        }
+
+        public void Dispose()
+        {
+            IssueService.Changed -= IssuesChanged;
+        }
+
+        private async Task LoadData()
+        {
+            _issues = await IssueService.GetAsync();
+            _milestones = CreateMilestones(_issues, _milestones);
+            _checkedIssues.Clear();
+        }
+
+        private async void IssuesChanged(object sender, EventArgs e)
+        {
+            await InvokeAsync(async () =>
+            {
+                await LoadData();
+                StateHasChanged();
+            });
+        }
+
+        private bool IsVisible(ApiReviewIssue issue)
+        {
+            if (_milestones != null && _milestones.TryGetValue(issue.Milestone, out var isChecked) && !isChecked)
+                return false;
+
+            if (string.IsNullOrEmpty(Filter))
+                return true;
+
+            if (issue.Title.Contains(Filter, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            if (issue.IdFull.Contains(Filter, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            if (issue.Author.Contains(Filter, StringComparison.OrdinalIgnoreCase))
+                return true;
+
+            foreach (var label in issue.Labels)
+            {
+                if (label.Name.Contains(Filter, StringComparison.OrdinalIgnoreCase))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private SortedDictionary<string, bool> CreateMilestones(IReadOnlyList<ApiReviewIssue> issues,
+                                                                SortedDictionary<string, bool> existingMilestones)
+        {
+            var result = new SortedDictionary<string, bool>();
+
+            foreach (var issue in issues)
+                result[issue.Milestone] = true;
+
+            if (existingMilestones != null)
+            {
+                foreach (var (k, v) in existingMilestones)
+                {
+                    if (result.ContainsKey(k))
+                        result[k] = v;
+                }
+            }
+
+            return result;
+        }
+
+        private void MilestoneChecked(string milestone)
+        {
+            if (_milestones.TryGetValue(milestone, out var isChecked))
+                _milestones[milestone] = !isChecked;
+        }
+
+        private async Task CopySelectedItems()
+        {
+            var text = GetMarkdown();
+            if (text == null)
+                return;
+
+            var html = Markdig.Markdown.ToHtml(text);
+            await JSRuntime.InvokeVoidAsync("clipboardCopy.copyText", text, html);
+            _checkedIssues.Clear();
+        }
+
+        private string GetMarkdown()
+        {
+            var sb = new System.Text.StringBuilder();
+
+            foreach (var issue in SelectedIssues)
+                sb.AppendLine($"* [{issue.IdFull}]({issue.Url}): {issue.Title}");
+
+            return sb.ToString();
+        }
+
+        private void CheckAllIssues(bool value)
+        {
+            if (value)
+                _checkedIssues.UnionWith(VisibleIssues);
+            else
+                _checkedIssues.ExceptWith(VisibleIssues);
+        }
+
+        private void CheckIssue(ApiReviewIssue issue, bool value)
+        {
+            if (value)
+                _checkedIssues.Add(issue);
+            else
+                _checkedIssues.Remove(issue);
+        }
+    }
+}
---
+++ ApiReview.Client/Pages/Notes.razor.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+
+using ApiReview.Client.Services;
+using ApiReview.Shared;
+
+namespace ApiReview.Client.Pages
+{
+    [Authorize(Roles = ApiReviewConstants.ApiApproverRole)]
+    public partial class Notes
+    {
+        [Inject]
+        private NotesService NotesService { get; set; }
+
+        private CancellationTokenSource _cts;
+        private DateTimeOffset _start;
+        private DateTimeOffset _end;
+
+        private DateTimeOffset Date
+        {
+            get => _start;
+            set
+            {
+                _start = value.Date.Add(_start.TimeOfDay);
+                _end = value.Date.Add(_end.TimeOfDay);
+                UpdateValidation();
+            }
+        }
+
+        private DateTimeOffset Start
+        {
+            get => _start;
+            set
+            {
+                _start = _start.Date.Add(value.TimeOfDay);
+                UpdateValidation();
+            }
+        }
+
+        private DateTimeOffset End
+        {
+            get => _end;
+            set
+            {
+                _end = _end.Date.Add(value.TimeOfDay);
+                UpdateValidation();
+            }
+        }
+
+        private string DateValidationMessage { get; set; }
+        private string StartValidationMessage { get; set; }
+        private string EndValidationMessage { get; set; }
+        private bool HasValidationErrors => DateValidationMessage != null ||
+                                            StartValidationMessage != null ||
+                                            EndValidationMessage != null;
+        private bool CanSearch => !HasValidationErrors && !IsLoading;
+
+        private bool IncludeVideo { get; set; }
+
+        private IReadOnlyList<ApiReviewVideo> Videos { get; set; } = Array.Empty<ApiReviewVideo>();
+        private ApiReviewVideo SelectedVideo { get; set; }
+
+        private bool IsLoading { get; set; }
+        private ApiReviewSummary Summary { get; set; }
+
+        private ApiReviewPublicationResult PublicationResult { get; set; }
+
+        protected override void OnInitialized()
+        {
+            _start = DateTime.Now.Date;
+            _end = _start.AddHours(23).AddMinutes(59);
+            IncludeVideo = true;
+        }
+
+        private void UpdateValidation()
+        {
+            if (Start.Date > DateTimeOffset.Now.Date)
+            {
+                DateValidationMessage = "Date cannot be in the future";
+                StartValidationMessage = null;
+            }
+            else
+            {
+                DateValidationMessage = null;
+
+                if (Start > DateTimeOffset.Now)
+                {
+                    StartValidationMessage = "Start cannot be in the future";
+                }
+                else
+                {
+                    StartValidationMessage = null;
+                }
+            }
+
+            if (End <= Start)
+            {
+                EndValidationMessage = "End Time must be after Start Time";
+            }
+            else
+            {
+                EndValidationMessage = null;
+            }
+        }
+
+        private async Task FindIssuesAsync()
+        {
+            IsLoading = true;
+            Videos = Array.Empty<ApiReviewVideo>();
+            SelectedVideo = null;
+            Summary = null;
+            PublicationResult = null;
+            StateHasChanged();
+
+            if (_cts != null)
+                _cts.Cancel();
+
+            _cts = new CancellationTokenSource();
+
+            var token = _cts.Token;
+
+            IReadOnlyList<ApiReviewVideo> videos = Array.Empty<ApiReviewVideo>();
+
+            if (IncludeVideo)
+            {
+                videos = await NotesService.GetVideos(Start, End);
+
+                if (token.IsCancellationRequested)
+                    return;
+            }
+
+            Videos = videos;
+            SelectedVideo = videos.FirstOrDefault();
+            StateHasChanged();
+
+            ApiReviewSummary summary;
+
+            if (SelectedVideo != null)
+                summary = await NotesService.IssuesForVideo(SelectedVideo.Id);
+            else
+                summary = await NotesService.IssuesForRange(Start, End);
+
+            if (token.IsCancellationRequested)
+                return;
+
+            Summary = summary;
+            IsLoading = false;
+        }
+
+        private async Task SelectVideoAsync(ApiReviewVideo video)
+        {
+            if (video == SelectedVideo)
+                return;
+
+            SelectedVideo = video;
+            Summary = null;
+            PublicationResult = null;
+            IsLoading = true;
+            StateHasChanged();
+
+            if (_cts != null)
+                _cts.Cancel();
+
+            _cts = new CancellationTokenSource();
+
+            var token = _cts.Token;
+
+            var summary = await NotesService.IssuesForVideo(video.Id);
+            if (token.IsCancellationRequested)
+                return;
+
+            Summary = summary;
+            IsLoading = false;
+        }
+
+        private async Task PublishNotesAsync()
+        {
+            IsLoading = true;
+
+            PublicationResult = await NotesService.PublishNotesAsync(Summary);
+
+            IsLoading = false;
+        }
+    }
+}
