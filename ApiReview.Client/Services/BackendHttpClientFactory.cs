17180bb50d4b942e2de39595c0edd08f1edf025b
Immo Landwerth <immol@microsoft.com>
2020-08-04 12:26:08 -0700

    Extract AuthenticatedBackendHttpClientFactory into a separate file

--- ApiReview.Client/Services/BackendHttpClientFactory.cs
+++ ApiReview.Client/Services/BackendHttpClientFactory.cs
@@ -6,17 +6,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Components.Authorization;
-
-using ApiReview.Shared;
-using Microsoft.Net.Http.Headers;
 
 namespace ApiReview.Client.Services
 {
     public class BackendHttpClientFactory
     {
         private readonly string _url;
-        private readonly AuthenticationStateProvider _provider;
 
         public BackendHttpClientFactory(IConfiguration configuration,
                                         IOptions<JsonOptions> jsonOptions)
@@ -35,31 +30,6 @@ namespace ApiReview.Client.Services
             };
 
             return Task.FromResult(client);
-        }
-    }
-
-    public sealed class AuthenticatedBackendHttpClientFactory : BackendHttpClientFactory
-    {
-        private readonly AuthenticationStateProvider _provider;
-
-        public AuthenticatedBackendHttpClientFactory(IConfiguration configuration,
-                                                     IOptions<JsonOptions> jsonOptions,
-                                                     AuthenticationStateProvider provider)
-            : base(configuration, jsonOptions)
-        {
-            _provider = provider;
-        }
-
-        public override async Task<HttpClient> CreateAsync()
-        {
-            var client = await base.CreateAsync();
-            var state = await _provider.GetAuthenticationStateAsync();
-            var token = state.User.FindFirst(ApiReviewConstants.TokenClaim)?.Value;
-
-            if (token != null)
-                client.DefaultRequestHeaders.Add(HeaderNames.Authorization, "Bearer " + token);
-
-            return client;
         }
     }
 }
