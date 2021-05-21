7c9b64570c67e021fe81ef3acb8c8459a378f446
Immo Landwerth <immol@microsoft.com>
2021-05-21 11:27:39 -0700

    Remove boilerplate comments

--- src/ApiReviewDotNet/Startup.cs
+++ src/ApiReviewDotNet/Startup.cs
@@ -31,8 +31,6 @@ namespace ApiReviewDotNet
         public IConfiguration Configuration { get; }
         public IWebHostEnvironment Env { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages().AddJsonOptions(o =>
@@ -96,7 +94,6 @@ namespace ApiReviewDotNet
                     });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -106,7 +103,6 @@ namespace ApiReviewDotNet
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
